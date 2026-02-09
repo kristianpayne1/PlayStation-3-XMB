efd0b096e5f41be028919be694b78149b6841c19
kristianpayne1 <kristianpayne1@gmail.com>
2026-02-09 17:22:04 +0000

    Implement gradient themes (CSS)

--- playstation-3-xmb/src/App.tsx
+++ playstation-3-xmb/src/App.tsx
@@ -3,7 +3,7 @@ import Wave from "./components/Wave";
 import { OrbitControls } from "@react-three/drei";
 
 function App() {
-    return <Canvas className="xmb-bg">
+    return <Canvas className="xmb-theme-bg-december">
         <Wave />
         <OrbitControls />
     </Canvas>;
